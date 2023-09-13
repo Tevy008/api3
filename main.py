d3fe48f7f59bfff12da1b4fcf4ea110ef44bfb6e
Tevy008 <110487893+Tevy008@users.noreply.github.com>
2023-09-13 16:28:47 +0300

    Оформил по pep8

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import argparse
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
+
 def shorten_link(bitly_key, link):
     url_bitlink = "https://api-ssl.bitly.com/v4/shorten"
     payload = {"long_url": link}
-    headers = {
-    "Authorization": bitly_key}
-    response = requests.post(url_bitlink, headers=headers,json=payload)
+    headers = {"Authorization": bitly_key}
+    response = requests.post(url_bitlink, headers=headers, json=payload)
     response.raise_for_status()
 
     return response.json()["id"]
@@ -18,8 +18,7 @@ def shorten_link(bitly_key, link):
 def count_clicks(bitly_key, bitlink):
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
     payload = {"units": -1, "unit": "month"}
-    headers = {
-    "Authorization": bitly_key}
+    headers = {"Authorization": bitly_key}
     response = requests.get(url, params=payload, headers=headers)
     response.raise_for_status()
     return response.json()["total_clicks"]
@@ -27,35 +26,29 @@ def count_clicks(bitly_key, bitlink):
 
 def is_bitlink(bitly_key, bitlink):
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
-    headers = {
-    "Authorization": bitly_key}
+    headers = {"Authorization": bitly_key}
     response = requests.get(url, headers=headers)
     return response.ok
-    
-    
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Программа помогает сокрашать ссылки а так же получать количество переходов по сокращенной ссылке')
-    parser.add_argument('--url', help='Введите ссылку:')
+    parser = argparse.ArgumentParser(
+        description="Программа помогает сокрашать ссылки а так же получать количество переходов по сокращенной ссылке"
+    )
+    parser.add_argument("--url", help="Введите ссылку:")
     args = parser.parse_args()
     parsed_url = urlparse(args.url)
-    url_parse = f"{parsed_url.netloc}{parsed_url.path}"
+    url_without_protocol = f"{parsed_url.netloc}{parsed_url.path}"
     load_dotenv()
-    bitly_key = os.environ['BITLY_KEY']
+    bitly_key = os.environ["BITLY_KEY"]
     try:
-        if is_bitlink(bitly_key,url_parse):
-            print(count_clicks(bitly_key,url_parse))
+        if is_bitlink(bitly_key, url_without_protocol):
+            print(count_clicks(bitly_key, url_without_protocol))
         else:
-            print(shorten_link(bitly_key,args.url))
+            print(shorten_link(bitly_key, args.url))
     except requests.exceptions.HTTPError as ex:
         print(ex)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-    
-
-
-
-
-    
